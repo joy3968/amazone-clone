dd318ac361aa8f3167351cfe09f3d88382c4bf86
kong-eunji <sasyo1z2@naver.com>
2022-07-03 03:21:29 +0900

    헤더 작업 및 파이어베이스 연동

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDGH8nG9JPz21fUqrS1SgOQmfRMSBEimfk",
+    authDomain: "e-clone-c1160.firebaseapp.com",
+    projectId: "e-clone-c1160",
+    storageBucket: "e-clone-c1160.appspot.com",
+    messagingSenderId: "970780566736",
+    appId: "1:970780566736:web:a557f31d038de11c7c2ad5",
+    measurementId: "G-67B07M88F6"
+  };  
